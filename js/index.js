ed8bff22958d47e8cf8617fb27545db810481637
Pratibha Goswami <goswami.pratibha88@gmail.com>
2024-09-10 22:57:22 -0700

    Open API work in Progress

--- js/index.js
+++ js/index.js
@@ -1,20 +1,41 @@
 
 
 
-async function getData() {
+async function getArtData() {
     try {
-        const response = await fetch('https://api.artic.edu/api/v1/artworks/129884');
+        const response = await fetch('https://api.artic.edu/api/v1/artworks');
         if (!response.ok) {
             throw new Error(response.status);
         }
-        const res = await response.json();
-        console.log(res);
+        const data = await response.json();
+        const artworks = data.data;
 
-    }
-    catch (error) {
-        console.log(error);
-    }
+        const container = document.getElementById('artworks-container');
+        container.innerHTML = '';
 
+        artworks.forEach(artwork => {
+            const artworkDiv = document.createElement('div');
+            artworkDiv.className = 'artwork';
+
+            const artworkTitle = document.createElement('h2');
+            artworkTitle.textContent = artwork.title;
+            artworkTitle.style.color = "#008080";
+
+            const artworkArtist = document.createElement('p');
+            artworkArtist.textContent = `Artist: ${artwork.artist_title}`;
+            artworkArtist.style.color = '#000080';
+
+            artworkDiv.appendChild(artworkTitle);
+            artworkDiv.appendChild(artworkArtist);
+
+            container.appendChild(artworkDiv);
+
+           
+        });
+
+    } catch (error) {
+        console.error('Error fetching artworks:', error);
+    }
 }
 
-getData()
+getArtData();
