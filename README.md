ecb4afc2f8860c22eda0402e13ecbd9219f8deb1
Pratibha Goswami <goswami.pratibha88@gmail.com>
2024-09-23 23:41:59 -0700

    Changes done for OPEN API Final project

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# pratibha_goswami_io
-Intro to Programming course with Code the Dream
+# Owner - Pratibha Goswami
+
+# Open API Project - ARTIC (Art Institute of Chicago)
+
+ARTIC is an art API from the Art Institute of Chicago. The Projects is about displaying the data for at least 2 of the models/data points in the API.
+In ARTIC open API Project , art images, Title, Artist , Art Institute Products and there Prices are displayed.
+
+## Table of Contents
+-[Requirements](#Requirements)
+-[Installation](#Installation)
+
+##Requirements
+
+VSCode
+GITHub 
+Browser
+
+##Installation
+
+Clone the repository and install dependencies:
+
+```bash
+
+git clone https://github.com/goswamipratibha88/pratibha_goswami_lo_open_api.git
+
+or
+
+git clone git@github.com:goswamipratibha88/pratibha_goswami_lo_open_api.git
+
+cd pratibha_goswami_lo_open_api
+
+open index.html in default browser of VSCode or in any other browser.
+
+
--- js/index.js
+++ js/index.js
@@ -1,41 +1,108 @@
 
-
-
-async function getArtData() {
+async function fetchProducts() {
     try {
-        const response = await fetch('https://api.artic.edu/api/v1/artworks');
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
+        const response = await fetch('https://api.artic.edu/api/v1/products?limit=6');
         const data = await response.json();
-        const artworks = data.data;
 
-        const container = document.getElementById('artworks-container');
-        container.innerHTML = '';
-
-        artworks.forEach(artwork => {
-            const artworkDiv = document.createElement('div');
-            artworkDiv.className = 'artwork';
-
-            const artworkTitle = document.createElement('h2');
-            artworkTitle.textContent = artwork.title;
-            artworkTitle.style.color = "#008080";
-
-            const artworkArtist = document.createElement('p');
-            artworkArtist.textContent = `Artist: ${artwork.artist_title}`;
-            artworkArtist.style.color = '#000080';
-
-            artworkDiv.appendChild(artworkTitle);
-            artworkDiv.appendChild(artworkArtist);
-
-            container.appendChild(artworkDiv);
-
-           
-        });
-
+        if (data && data.data) {
+            displayProducts(data.data);
+        }
     } catch (error) {
-        console.error('Error fetching artworks:', error);
+        console.error('Error fetching data:', error);
     }
 }
 
-getArtData();
+function displayProducts(productData) {
+
+    const container = document.getElementById('product-container');
+    container.innerHTML = '';
+
+    productData.forEach(product => {
+
+        const productDiv = document.createElement('div');
+        productDiv.className = 'product-piece';
+
+        const prodTitle = document.createElement('h3');
+        prodTitle.innerText = "Name:  " + product.title;
+
+        const prodImage = document.createElement('img');
+        prodImage.src = product.image_url;
+        prodImage.alt = product.title || 'Product Image';
+
+        const prodPrice = document.createElement('p');
+        prodPrice.innerText = "Price:  $" + product.min_current_price;
+
+        productDiv.appendChild(prodTitle);
+        productDiv.append(prodImage);
+        productDiv.appendChild(prodPrice);
+        container.appendChild(productDiv);
+    });
+
+}
+
+// fetchProducts() - To display products under product tab
+
+fetchProducts();
+
+let currentPage = 1;
+
+async function fetchArtworks(page) {
+    try {
+        const response = await fetch(`https://api.artic.edu/api/v1/artworks?page=${page}&limit=10`);
+        const data = await response.json();
+
+        return data.data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+async function displayArtworks() {
+    const arts = await fetchArtworks(currentPage);
+    const container = document.getElementById('artworks-container');
+    container.innerHTML = '';
+    arts.forEach(art => {
+
+        const artDiv = document.createElement('div');
+        artDiv.className = 'art-piece';
+
+        const title = document.createElement('h2');
+        title.innerText = art.title || 'Untitled';
+
+        //ID cn be used to display image id
+        // const ID = document.createElement('h2');
+        // ID.innerText = art.image_id|| 'Untitled';
+
+        const artistDisplay = document.createElement('p');
+        artistDisplay.innerText = art.artist_display;
+
+        const image = document.createElement('img');
+        image.src = `https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`;
+        image.alt = art.title || 'Artwork Image';
+
+        if (art.title !== 'Untitled') {
+
+            artDiv.appendChild(image);
+            artDiv.appendChild(title);
+            artDiv.appendChild(artistDisplay);
+            container.appendChild(artDiv);
+        }
+
+    });
+
+}
+
+//For Next Button
+document.getElementById('nextPageButton').addEventListener('click', () => {
+    currentPage++;
+    displayArtworks();
+});
+
+//For Back button
+document.getElementById('backPageButton').addEventListener('click', () => {
+    currentPage--;
+    displayArtworks();
+});
+
+//displayArtworks() - To display art details under artworks tab
+displayArtworks();
